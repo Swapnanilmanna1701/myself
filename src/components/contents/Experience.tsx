1b22bda0e0e94fa6c2d4024b6e8ff147cc444590
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-31 17:14:28 +0530

    section addition

---
+++ src/components/contents/Experience.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TimeLine, TimeLineItem } from '../utils/ExpTimeLine';
+
+const Experience = () => {
+    return (
+        <TimeLine>
+            <TimeLineItem active>
+                <TimeLineItem.Title>
+                    <span className="font-medium transition-all hover:text-white hover:underline hover:underline-offset-2">
+                        Frontend Developer at Polymath AI
+                    </span>
+                    {" "}•{" "}
+                    <span className="text-neutral-200">
+                        June 2024 - August 2024
+                    </span>
+                </TimeLineItem.Title>
+                <TimeLineItem.Description>
+                    Working as a frontend developer at Polymath AI, contributing to innovative solutions in the AI space.
+                </TimeLineItem.Description>
+            </TimeLineItem>
+
+            <TimeLineItem last>
+                <TimeLineItem.Title>
+                    <span className="font-medium transition-all hover:text-white hover:underline hover:underline-offset-2">
+                        Backend Developer (Freelance) at Saman Technosys
+                    </span>
+                    {" "}•{" "}
+                    <span className="text-neutral-200">
+                        Oct 2023 - Nov 2023
+                    </span>
+                </TimeLineItem.Title>
+                <TimeLineItem.Description>
+                    Developed scripts with Node.js to get scrap data of profiles and store in excel.
+                </TimeLineItem.Description>
+            </TimeLineItem>
+        </TimeLine>
+    );
+};
+
+export default Experience;
---
+++ src/lib/data.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+
+export const links = [
+    {
+        name: "Home",
+        hash: "/",
+    },
+    {
+        name: "About",
+        hash: "/about",
+    },
+    {
+        name: "Projects",
+        hash: "/projects",
+    },
+    {
+        name: "Blog",
+        hash: "/blog",
+    },
+    {
+        name: "Contact",
+        hash: "/contact",
+    },
+] as const;
